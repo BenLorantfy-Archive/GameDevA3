3a620820d5726906342fd3ff7a57076dc0265f7f
Ben <benlorantfy@gmail.com>
2016-12-02 15:30:13 -0500

    added floating platform

---
+++ UnityGamePlayer/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+	private bool movingRight = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (movingRight) {
+			this.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z - 1 * Time.deltaTime);
+			if (this.transform.position.z < - 11) {
+				movingRight = false;
+			}
+		} else {
+			this.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z + 1 * Time.deltaTime);
+			if (this.transform.position.z > -9) {
+				movingRight = true;
+			}	
+		}
+
+		Debug.Log (this.transform.position.z);
+
+	}
+}
