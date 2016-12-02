ecaf62217749dd5dd2d446d58cd1c7b1f3bbd48f
Ben <benlorantfy@gmail.com>
2016-12-02 03:08:56 -0500

    added ground models, fixed camera, moved unneeded stuff to separate folder

---
+++ UnityGamePlayer/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	public GameObject mainCamera = null;
+	public GameObject player = null;
+	float deltaY = 0;
+
+	// Use this for initialization
+	void Start () {
+		deltaY = mainCamera.transform.position.y - player.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mainCamera.transform.position = new Vector3 (mainCamera.transform.position.x, player.transform.position.y + deltaY, player.transform.position.z);
+
+	}
+}
